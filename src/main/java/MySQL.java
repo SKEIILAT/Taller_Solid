5cbc952f9e6b31a22a607a8096bc46129c8979e5
Javier Gutiérrez <javaguti@espol.edu.ec>
2024-10-16 21:45:16 -0500

    Initial loading of the code

---
+++ src/main/java/MySQL.java
@@ -0,0 +1,9 @@
+public class MySQL {
+    public void insert(String statement){}
+    public void select(String statement){}
+    public void delete(String statement){}
+    public void update(String statement){}
+    // More Methods
+}
+
+
---
+++ src/main/java/com/mycompany/taller_solid_base/Taller_Solid_Base.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller_solid_base;
+
+/**
+ *
+ * @author USER
+ */
+public class Taller_Solid_Base {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
